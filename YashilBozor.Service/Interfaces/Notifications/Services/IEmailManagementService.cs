b55698ded748ae402da34cdaee891943785c01bb
Habiba0206 <habibasattorova26@gmail.com>
2024-02-11 00:52:37 +0500

    created all notification interfaces

---
+++ YashilBozor.Service/Interfaces/Notifications/Services/IEmailManagementService.cs
@@ -0,0 +1,7 @@
+﻿namespace YashilBozor.Service.Interfaces.Notifications.Services;
+
+public interface IEmailManagementService
+{
+    ValueTask<bool> SendEmailAsync(string senderUserEmail, string receieverUserEmail, Guid templateId, string code = "");
+    ValueTask<bool> SendEmailAsync(string senderUserEmail, string receieverUserEmail, string templateCategory, string code="");
+}
---
+++ YashilBozor.Service/Interfaces/Notifications/Services/IEmailSenderService.cs
@@ -0,0 +1,8 @@
+﻿using YashilBozor.Service.Models;
+
+namespace YashilBozor.Service.Interfaces.Notifications.Services;
+
+public interface IEmailSenderService
+{
+    Task<bool> SendEmailAsync(EmailMessage emailMessage);
+}
