301571ee8859f5c05baee5028cd48b437ea7addb
Habiba0206 <habibasattorova26@gmail.com>
2024-02-11 00:52:03 +0500

    created all category interfaces

---
+++ YashilBozor.Service/Interfaces/Categories/ICategoryService.cs
@@ -0,0 +1,33 @@
+﻿using YashilBozor.Domain.Configurations;
+using YashilBozor.Service.DTOs.Categories;
+
+namespace YashilBozor.Service.Interfaces.Categories;
+
+public interface ICategoryService
+{
+    ValueTask<IEnumerable<CategoryForResultDto>> GetAllAsync(
+        PaginationParams @params,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<CategoryForResultDto> GetByIdAsync(
+        Guid categoryId,
+        bool asNoTracking = false,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<CategoryForResultDto> CreateAsync(
+        CategoryForCreationDto categoryForCreationDto,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<CategoryForResultDto> UpdateAsync(
+        CategoryForUpdateDto categoryForUpdateDto,
+        Guid categoryId,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+
+    ValueTask<CategoryForResultDto> DeleteAsync(
+        Guid categoryId,
+        bool saveChanges = true,
+        CancellationToken cancellationToken = default);
+}
