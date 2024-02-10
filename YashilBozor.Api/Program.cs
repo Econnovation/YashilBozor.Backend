33e9a16a322f7fc2f67a47a9b8a3118c62af8ac4
Habiba0206 <habibasattorova26@gmail.com>
2024-02-11 00:45:26 +0500

    added becessary packages and project references

--- YashilBozor.Api/Program.cs
+++ YashilBozor.Api/Program.cs
@@ -1,25 +1,13 @@
+using YashilBozor.API.Configurations;
+using YashilBozor.Service.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+await builder.ConfigureAsync();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+WebHostEnviromentHelper.WebRootPath = Path.GetFullPath("wwwroot");
 
-app.UseHttpsRedirection();
+await app.ConfigureAsync();
 
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await app.RunAsync();
