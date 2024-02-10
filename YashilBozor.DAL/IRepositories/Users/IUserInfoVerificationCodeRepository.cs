f58a2f26375fcf251d314b96180cce3052e2e4bf
Habiba0206 <habibasattorova26@gmail.com>
2024-02-10 23:58:39 +0500

    Created repository interfaces for all entities

---
+++ YashilBozor.DAL/IRepositories/Users/IUserInfoVerificationCodeRepository.cs
@@ -0,0 +1,12 @@
+﻿using YashilBozor.DAL.IRepositories.Commons;
+using YashilBozor.Domain.Entities.Users;
+
+namespace YashilBozor.DAL.IRepositories.Users;
+
+public interface IUserInfoVerificationCodeRepository : IRepository<UserInfoVerificationCode>
+{
+    ValueTask DeactivateAsync(
+        Guid codeId, 
+        bool saveChanges = true, 
+        CancellationToken cancellationToken = default);
+}
